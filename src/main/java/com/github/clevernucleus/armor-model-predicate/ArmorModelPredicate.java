f64fb487b182f4d102c779f0b5fc6e91879adc1e
CleverNucleus <jamie@mclean007.plus.com>
2021-10-04 08:30:25 +0100

    Setup

---
+++ src/main/java/com/github/clevernucleus/armor-model-predicate/ArmorModelPredicate.java
@@ -0,0 +1,11 @@
+package com.github.clevernucleus.armor-model-predicate;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		
+	}
+}
